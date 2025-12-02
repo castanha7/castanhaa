dee8450ccce83dc42ae23d4ec8148257a5373dcf
castanha7 <vinicius.calegari@escola.pr.gov.br>
2025-12-02 18:42:01 -0300

    s

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// Confirma que o script foi carregado corretamente
+console.log("Site 'Castanha' carregado com sucesso!");
 
+// Você pode adicionar interatividade aqui no futuro.
+// Exemplo: Mudar algo quando o usuário clicar em um botão.
